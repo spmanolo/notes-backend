fa17b364e585156db1bf160ab1994887a865fef7
spmanolo <manolosan2001@gmail.com>
2023-09-03 23:46:05 +0200

    fixed env config

--- utils/config.js
+++ utils/config.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
-// const { PORT, MONGODB_URI, TEST_MONGODB_URI, NODE_ENV } = process.env
+const { PORT, MONGODB_URI, TEST_MONGODB_URI, NODE_ENV } = process.env
 
-const port = process.env.PORT
-let connectionString = process.env.MONGODB_URI
-if (process.env.NODE_ENV === 'test') {
-  connectionString = process.env.TEST_MONGODB_URI
+const port = PORT
+let connectionString = MONGODB_URI
+if (NODE_ENV === 'test') {
+  connectionString = TEST_MONGODB_URI
 }
 
 module.exports = {
