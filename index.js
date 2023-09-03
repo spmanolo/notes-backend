c492b0d64e86b77c1a5e2ccd1df9a95bbe7130f4
spmanolo <manolosan2001@gmail.com>
2023-09-03 22:59:53 +0200

    fixed index

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const app = require('./app.js')
 const config = require('./utils/config.js')
 const logger = require('./utils/logger.js')
 
-const port = process.env.PORT || config.port
-
-const server = app.listen(port, () => {
-  logger.info(`Server running on port ${port}`)
+const server = app.listen(config.port, () => {
+  logger.info(`Server running on port ${config.port}`)
 })
 
 module.exports = {
